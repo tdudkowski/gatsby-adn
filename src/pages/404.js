6304dc4f93534beed263f644fac9a7ede5ebc725
tdudkowski <tdudkowski@gmail.com>
2021-03-03 03:59:06 +0100

    update to Gatsby 3.0

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Layout from "../components/layout";
 
-export default () => {
+const NotFound = () => {
   return (
     <Layout>
       <h1>404</h1>
     </Layout>
   );
 };
+
+export default NotFound
